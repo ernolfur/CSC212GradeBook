4562e84e19cb3dddaebd2c3e9de49b8cbef685bb
Vex <117248789+VioletVex@users.noreply.github.com>
2024-04-22 15:56:26 -0400

    Uploaded SLNode.h

---
+++ SkipList_type_T/SLNode.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+
+template <typename N>
+class Node{
+private:
+    N data;
+    std::vector<Node*> nexts;
+
+    template <typename T>
+    friend class SkipList;
+public:
+    Node();
+    // Overloading
+    Node(N data);
+    Node(N data, Node* next);
+    ~Node();
+};
+
+// Default constructor
+template <typename N>
+Node<N>::Node(){
+    this->data = 0;
+    this->nexts.push_back(nullptr);
+}
+
+template <typename N>
+Node<N>::Node(N data){
+    this->data = data;
+    this->nexts.push_back(nullptr);
+}
+
+template <typename N>
+Node<N>::Node(N data, Node* next){
+    this->data = data;
+    this->nexts.push_back(next);
+}
+
+template <typename N>
+Node<N>::~Node(){
+    this->nexts.clear();
+}
