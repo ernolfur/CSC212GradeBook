deb7f5ed8e3e13e662076ed5fcd32a37eb086ee7
Vex <117248789+VioletVex@users.noreply.github.com>
2024-04-22 16:02:56 -0400

    Removed debug output from main.cpp

--- SkipList_type_T/main.cpp
+++ SkipList_type_T/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[]){
     //srand(static_cast<int>(1));
 
     SkipList<int>* SL;
-    std::string file_name(argv[1]);
-    //std::string file_name("test.txt");
+    //std::string file_name(argv[1]);
+    std::string file_name("test.txt");
 
     std::vector<int> file_data;
     std::string line;
@@ -33,9 +33,7 @@ int main(int argc, char* argv[]){
     std::cout<< SL->full_print() <<std::endl;
     SL->remove(52);
     std::cout<< "Removed 52" <<std::endl;
-    std::cout<< "Final:\n";
     std::cout<< SL->to_string() <<std::endl;
     std::cout<< SL->full_print() <<std::endl;
-    std::cout<< "No Crash" <<std::endl;
     return 0;
 }
